5707b9a5bb28e5d8d23ff734f022e2d5653a6d35
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 13:57:57 -0700

    add func to print all user info

--- user.py
+++ user.py
@@ -3,6 +3,13 @@
 # By:   Virgilio D. Cabading Jr.    Created: October 26, 2021
 # *********************************************************
 
+def print_all_users_info (user_array):                      # Go through an array of users and print their info
+    for idx in range(len(user_array)):
+        user_array[idx].info()
+
+def print_desc(description:str):
+    print(f"{f' {description} ':*^80}\n")
+
 # //// CLASS USER /////////////////////////////////////////
 class User:
 
@@ -37,18 +44,20 @@ class User:
 # //// MAIN EXECUTABLE SECTION ////////////////////////////
 
 user = []
-user.append(User())                                         # Create 2 objects
+user.append(User())                                         # Create 3 users
+user.append(User())
 user.append(User())
 
 user[0].update_user_info("Vin", "Diesel", 1000)             # Initialize object their information
 user[1].update_user_info("Brad", "Pitt", 1000)
+user[2].update_user_info("Dwayne", "Johnson", 1000)
 
-user[0].info()                                              # Print User Info
-user[1].info()
+print_all_users_info(user)
 
-print(f"{f' Withdrawing 200 from Vin Diesel ':*^80}\n")
+print_desc("Withdrawing 200 from Vin Diesel")               # Make a withdrawal
+print(f"{f' Withdrawing 200 from Vin Diesel ':*^80}\n")     # Make a withdrawal
 user[0].make_withdrawal(200)
 
-user[0].display_user_balance()
+user[0].display_user_balance()                              # Display updated user balance
 user[1].display_user_balance()
 
