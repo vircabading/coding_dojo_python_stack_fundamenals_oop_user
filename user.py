16835eb9971e934189a3aaebfcfdd95d1435d56f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 12:37:13 -0700

    add method make_withdrawal

--- user.py
+++ user.py
@@ -4,18 +4,28 @@
 # *********************************************************
 
 # //// CLASS USER /////////////////////////////////////////
-from _typeshed import Self
-
-
 class User:
 
     # **** CLASS METHODS **********************************
     def __init__(self) -> None:                             # Class Instance Constructor
+        self.first_name = "John"
+        self.last_name = "Doe"
         self.account_balance = 0;
     
-    def make_withdrawal (self, amount) -> Self:             # Method that decreases the user's account_balance by specified amount
+    def make_withdrawal (self, amount):             # Method that decreases the user's account_balance by specified amount
         self.account_balance -= amount
+        return self
+    
+    def info(self):
+        print(f"{'::::: {self.first_name} {self.last_name} Information :::::':^60}")
+        print(f"first_name : {self.first_name}")
+        print(f"last_name : {self.last_name}")
+        print(f"account_balance: {self.account_balance}")
         return self
 
 # //// MAIN EXECUTABLE SECTION ////////////////////////////
 
+Virgilio = User()
+Virgilio.info().account_balance = 1000
+Virgilio.info().make_withdrawal(200)
+Virgilio.info()
