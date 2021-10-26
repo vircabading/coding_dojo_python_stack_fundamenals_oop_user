b8f8eabf9bb0ab7b83965a07cf0f267bd7fd78fb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 12:18:10 -0700

    add make withdrawal  method

--- user.py
+++ user.py
@@ -4,7 +4,18 @@
 # *********************************************************
 
 # //// CLASS USER /////////////////////////////////////////
+from _typeshed import Self
+
+
 class User:
+
+    # **** CLASS METHODS **********************************
     def __init__(self) -> None:                             # Class Instance Constructor
-        pass
+        self.account_balance = 0;
     
+    def make_withdrawal (self, amount) -> Self:             # Method that decreases the user's account_balance by specified amount
+        self.account_balance -= amount
+        return self
+
+# //// MAIN EXECUTABLE SECTION ////////////////////////////
+
