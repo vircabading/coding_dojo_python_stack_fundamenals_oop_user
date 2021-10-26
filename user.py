e3f0f00657b745986329f797b801d60fc0df1b0b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 13:11:49 -0700

    intitialize 2 user info

--- user.py
+++ user.py
@@ -16,13 +16,19 @@ class User:
         self.account_balance -= amount
         return self
     
-    def display_user_balance(self):
+    def display_user_balance(self):                         # Method display user balance
         print(f"{' Balance Information ':*^80}")
         print(f" User: {self.first_name} {self.last_name} ::: Balance: {self.account_balance}\n")
         return self
+    
+    def update_user_info (self, first_name="John", last_name="Doe", account_balance=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.account_balance = account_balance
+        return self
 
     def info(self):                                         # Method that displays info of class instance
-        print(f"{' {self.first_name} {self.last_name} Information ':*^80}")
+        print(f"{f' {self.first_name} {self.last_name} Information ':*^80}")
         print(f"first_name : {self.first_name}")
         print(f"last_name : {self.last_name}")
         print(f"account_balance: {self.account_balance}\n")
@@ -30,7 +36,19 @@ class User:
 
 # //// MAIN EXECUTABLE SECTION ////////////////////////////
 
-Virgilio = User()
-Virgilio.info().account_balance = 1000
-Virgilio.info().make_withdrawal(200)
-Virgilio.display_user_balance()
+user = []
+user.append(User())                                         # Create 2 objects
+user.append(User())
+
+user[0].update_user_info("Vin", "Diesel", 1000)             # Initialize object their information
+user[1].update_user_info("Brad", "Pitt", 1000)
+
+user[0].info()                                              # Print User Info
+user[1].info()
+
+print(f"{f' Withdrawing 200 from Vin Diesel ':*^80}\n")
+user[0].make_withdrawal(200)
+
+user[0].display_user_balance()
+user[1].display_user_balance()
+
