2e9e6351892a0e2ede875fa29921df55d944a958
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 12:10:59 -0700

    initialize

---
+++ user.py
@@ -0,0 +1,10 @@
+# *********************************************************
+# Subj: Coding Dojo > Python > Fundamentals: User
+# By:   Virgilio D. Cabading Jr.    Created: October 26, 2021
+# *********************************************************
+
+# //// CLASS USER /////////////////////////////////////////
+class User:
+    def __init__(self) -> None:                             # Class Instance Constructor
+        pass
+    
